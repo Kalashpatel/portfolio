74ae015b12938d8c895f12a75b696c259ef7cc4b
Kalashpatel <kalashpatel606@gmail.com>
2026-02-23 16:43:23 +0530

    preloader

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,11 +1,16 @@
-window.addEventListener("load", function() {
-    const preloader = document.getElementById("preloader");
-    
-    // Smoothly fade out
-    preloader.classList.add("preloader-hidden");
-    
-    // Optional: Remove from DOM after fade to save memory
-    setTimeout(() => {
-        preloader.style.display = "none";
-    }, 500);
-});
+// Function to hide the loader
+function hidePreloader() {
+    const preloader = document.getElementById('preloader');
+    if (preloader) {
+        preloader.style.opacity = '0';
+        preloader.style.visibility = 'hidden';
+        document.body.classList.remove('loading');
+    }
+}
+
+// 1. Hide when everything is finished (images, videos, etc.)
+window.addEventListener('load', hidePreloader);
+
+// 2. SAFETY: If the page takes more than 5 seconds, hide it anyway 
+// so the user isn't stuck on a black screen.
+setTimeout(hidePreloader, 5000);
